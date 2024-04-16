5b1895191b0752a2150c85a5813c5b7028389fe4
Leandra Chevrier <chevrier.devexpert@outlook.com>
2024-04-16 17:10:13 +0200

    Git file added

---
+++ chuck-joke-generator-start/script.js
@@ -0,0 +1,26 @@
+// Variables 
+const jokeBtn = document.getElementById('joke-btn');
+const displayJoke = document.getElementById('joke');
+
+// Functions
+function generateJoke(){
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4){
+            if (xhr.status === 200){
+                displayJoke.innerHTML = JSON.parse(this.responseText).value;
+            } else {
+                displayJoke.innerHTML = 'Something Went Wrong (Not Funny)';
+            }
+        }
+    }
+
+    xhr.send();
+};
+
+// Event Listeners
+jokeBtn.addEventListener('click', generateJoke);
+document.addEventListener('DOMContentLoaded', generateJoke);
